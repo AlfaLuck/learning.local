b66461c12f62d1f3bf096eddfb5a69b5f2fc83c9
AlfaLuck <alfaluck@gmail.com>
2013-12-22 18:20:25 +0200

    Создание учебного HelloWorld 2

--- module/Helloworld/src/Helloworld/Controller/IndexController.php
+++ module/Helloworld/src/Helloworld/Controller/IndexController.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
-namespace Application\Controller;
+namespace Helloworld\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
@@ -16,6 +9,6 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        return new ViewModel(array('greeting' => 'Hello world!'));
     }
 }
